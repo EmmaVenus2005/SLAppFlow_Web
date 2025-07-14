b55ddc594e7d0dd0f25fbe9826fa521593e6f1d2
Emma Venus <emmavenus2005@gmx.com>
2025-07-14 12:48:52 +0000

    Checking if the connected user is admin, doesn't include the app if not

--- apps/0000_ProdigiesArtGallery/web/include.php
+++ apps/0000_ProdigiesArtGallery/web/include.php
@@ -3,5 +3,5 @@
 // Include file, must return true if the app can be seen in the menu
 // Use usual public commands to determine whether it should appear or not
 
-// Appears in all cases
-return true;
+// Checks if the user is an admin of the art gallery
+return AFSendFlowMessage(AFGetAppID(), "Global", "ISADMIN");
