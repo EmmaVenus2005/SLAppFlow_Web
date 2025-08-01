06f4b8bddae32cd4084314eca41a9d33fc17c229
Emma Venus <emmavenus2005@gmx.com>
2025-08-01 11:35:30 +0000

    On WebControl, added BestBid column and display the value for the active auction

--- apps/0000_ProdigiesArtGallery/flows/on_flow_message.php
+++ apps/0000_ProdigiesArtGallery/flows/on_flow_message.php
@@ -579,6 +579,20 @@ if ($messageParts[0] === "LISTUNICATS" && IsAdmin(AFGetSenderID()))
         $jsonInfo = NVGetList("Information", $unicat);
         $info = json_decode($jsonInfo, true);
 
+        // Get the best bid amount using the helper (returns an array of [timestamp, json])
+        $bestBidArr = GetBestBids($unicat, 1);
+
+        // Default best bid to null or 0
+        $bestBid = null;
+
+        if ($bestBidArr && isset($bestBidArr[0][1])) {
+            $bidJson = json_decode($bestBidArr[0][1], true);
+            $bestBid = $bidJson['amount'] ?? null;
+        }
+
+        // Add the best_bid parameter to info
+        $info['best_bid'] = $bestBid;
+
         // Merge the UNICAT number into the output array
         $output[] = array_merge(['number' => $unicat], $info);
 
